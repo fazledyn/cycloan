10af3f9979ab3e5f4d5054a6e8a33feace7ebb38
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2020-10-28 21:22:45 +0600

    / problem minor

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-    path('/',   views.LoginView.as_view(),   name='login-view'),
+    path('',   views.LoginView.as_view(),   name='login-view'),
 ]
