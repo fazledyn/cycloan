eaca9bb621be3eed063e6b343d6f2e4525dafd47
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2020-10-29 21:33:38 +0600

    session test in views added. test passed

--- core/views.py
+++ core/views.py
@@ -5,4 +5,11 @@ from django.views import View
 class LoginView(View):
     
     def get(self, request):
+
+        name = "Admin"
+        request.session['UserName'] = name
+        print(request.session['UserName'])
+        print('\n\n')
+        print(request.session)
+
         return render(request, 'index.html')
