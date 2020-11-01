1414d49f452f5dc2e7484481bca0a1056e73d3d6
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2020-11-02 02:17:30 +0600

    Django unwanted core apps rmoved

--- core/views.py
+++ core/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
 
-
 class LoginView(View):
-    
     def get(self, request):
-
-        name = "Admin"
-        request.session['UserName'] = name
-        print(request.session['UserName'])
-        print('\n\n')
-        print(request.session)
-
         return render(request, 'index.html')
--- cycloan/settings.py
+++ cycloan/settings.py
@@ -29,14 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
@@ -49,7 +43,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -65,7 +58,6 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -73,7 +65,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'cycloan.wsgi.application'
-
 
 # Sessions Config
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
