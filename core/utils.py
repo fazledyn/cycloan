db7e821209e63b3f70636de358d7eb32b74521a3
Ataf Fazledin <1705066@ugrad.cse.buet.ac.bd>
2020-11-02 03:11:56 +0600

    [ NOT TESTED ] utility functions for auth added

---
+++ core/utils.py
@@ -0,0 +1,58 @@
+from django.utils.decorators import wraps
+from django.shortcuts import redirect
+from django.contrib import messages
+from cycloan.settings import SECRET_KEY
+
+from datetime import datetime, timedelta
+import jwt
+
+
+def verify_auth_token(func):
+    @wraps(func)
+    def wrapped(request, *args, **kwargs):
+        auth_token = request.session['auth_token']
+        if not auth_token:
+            messages.warning(request, 'Session expired. Please log in again.')
+            return redirect('login-view')
+        
+        try:
+            auth_data = jwt.decode(auth_token, SECRET_KEY, algorithms=['HS256'])
+        except:
+            messages.warning(request, 'Session expired. Please log in again.')
+            return redirect('login-view')
+        return func(*args, **kwargs)
+
+    return wrapped
+
+
+def create_auth_token(user_id):
+    auth_token = jwt.encode(
+        {
+            'user_id': user_id,
+            'exp': datetime.now() + timedelta(seconds=600000)
+        }, SECRET_KEY, algorithm='HS256'
+    )
+    return auth_token
+
+
+def check_customer(func):
+    @wraps(func)
+    def wrapped(request, *args, **kwargs):
+        if request.session['user_type'] != 'customer':
+            messages.warning(request, 'You are not allowed to view that page.')
+            return redirect('owner-dashboard-view')
+
+        return func(*args, **kwargs)
+    return wrapped
+
+
+def check_owner(func):
+    @wraps(func)
+    def wrapped(request, *args, **kwargs):
+        if request.session['user_type'] != 'owner':
+            messages.warning(request, 'You are not allowed to view that page.')
+            return redirect('customer-dashboard-view')
+
+        return func(*args, **kwargs)
+    return wrapped
+
