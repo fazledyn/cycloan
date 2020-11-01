ad52bea79a8efe00ce96ae2dc843c1e83951f040
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2020-11-02 00:53:52 +0600

    File Upload handled. Work left to do.

--- customer/views.py
+++ customer/views.py
@@ -41,14 +41,33 @@ class CustomerRegisterView(View):
         return render(request, 'customer_register.html')
 
     def post(self, request):
-        # eitar kaaj kora lagbe
-        pass
+        
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+        
+        fullname = request.POST.get('fullname')
+        contact = request.POST.get('contact')
+        doctype = request.POST.get('doctype')
+        document = request.FILES.get('document')
+
+        print(document.name)
+        print('----')
+        filename = 'files/doc/user_' + email + document.name
+        print(filename)
+
+        file = open(filename, 'wb')
+        for chunk in document.chunks():
+            file.write(chunk)
+        file.close()
+
+        return redirect('cutomer-dashboard-view')
 
 
 class CustomerDashboardView(View):
 
     def get(self, request):
-        customer_id = request.session['customer_id']
+        customer_id = request.session.get('customer_id')
         cursor = connection.cursor()
         sql = "SELECT CUSTOMER_NAME FROM CUSTOMER WHERE CUSTOMER_ID=%s"
         cursor.execute(sql, [customer_id])
